eba92253b8def714d09b501d0b145a9675461a20
Teobald Pamba <teobaldpamba@gmail.com>
2023-10-11 12:22:18 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Retrun: Always 0 (success)
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		if (alpha == 'q' && alpha =='e')
+		{
+			alpha++;
+			continue;
+		}
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
