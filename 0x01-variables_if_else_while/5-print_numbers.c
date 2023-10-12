91266de1620d99f48a44074c36a8db1635f84517
Teobald Pamba <teobaldpamba@gmail.com>
2023-10-12 10:45:31 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int dig = 0;
+
+	while (dig <= 10)
+	{
+		putchar(dig + '0');
+		dig++;
+	}
+	putchar('\n');
+	return (0);
+}
