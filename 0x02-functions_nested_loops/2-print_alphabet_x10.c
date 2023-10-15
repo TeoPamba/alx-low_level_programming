30719cee4a9e11db5bc2bc53ce98394d5810e86b
Teobald Pamba <teobaldpamba@gmail.com>
2023-10-15 15:12:28 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets ten times
+ * Return: Nothing
+ */
+void print_alphabet_x10(void)
+{
+	int num;
+	char letter;
+
+	for (num = 0; num < 10; num++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+	}
+	_putchar('\n');
+}
