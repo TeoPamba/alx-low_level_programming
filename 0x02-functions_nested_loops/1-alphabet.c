47b91e429faf48043532efc9143be5c96b74bf92
Teobald Pamba <teobaldpamba@gmail.com>
2023-10-15 14:49:10 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Print all alphabets in lover case
+ * @c: character to be printed
+ * Return: return nothing 
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
