5ebc67a32838c9eae37ebb58de2fbf8ed9b18228
Teobald Pamba <teobaldpamba@gmail.com>
2023-10-19 12:00:29 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,8 @@
  * print_sign - checks numbers
  * @n: number checked
  * Return: 1 if positive, 0 if zero and -1 if negative
- */int print_sign(int n)
+ */
+int print_sign(int n)
 {
 	if (n > 0)
 	{
