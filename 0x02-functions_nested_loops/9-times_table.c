9721f04665bfc0a096ef4f9617a245239b30f11d
Teobald Pamba <teobaldpamba@gmail.com>
2023-10-20 13:13:04 -0700

    9-times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - gives a multiplication
+ */
+void times_table(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			int mult = a * b;
+
+			printf("%d, ", mult);
+		}
+		printf("\n");
+	}
+}
