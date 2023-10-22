cb5e5a2614711aab943730b440e14f04b17ff9bf
Teobald Pamba <teobaldpamba@gmail.com>
2023-10-22 14:44:57 -0700

    7-diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,10 +3,10 @@
  * print_diagonal - print disgonals
  * @n: number of spaces
  */
-void print_diagonal( int n)
+void print_diagonal(int n)
 {
 	int i, j;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
