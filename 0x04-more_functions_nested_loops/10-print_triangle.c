e2064efcd833c34e087104fdf531b13f69d716a3
Teobald Pamba <teobaldpamba@gmail.com>
2023-10-23 02:42:40 -0700

    7-diagonal

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_triangle - prints riangle shape based on size
+ * @size: input for size of triangle
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i <= size; i++)
+		for (
